b920bfa2844c7cfbc90c293035ed61b2d8817a24
Hasibul Hasan Shanto <63589669+shaa2020@users.noreply.github.com>
2025-04-16 00:19:04 +0600

    Delete spx-protocol directory

--- spx-protocol/run_client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# run_client.sh
-# SPX Protocol client by Shaan (github.com/shaa2020)
-python -m spx.client
--- spx-protocol/spx/__init__.py
+++
@@ -1,4 +0,0 @@
-# spx/_init_.py
-# SPX Protocol by Shaan (github.com/shaa2020)
-_version_ = "5.0.0"
-_legend_ = "A cosmic masterpiece by Shaan"
